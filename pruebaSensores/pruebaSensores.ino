e93aae98efc208fce328e8e36043485644f9186d
BenEspinoza10 <50683392+BenEspinoza10@users.noreply.github.com>
2024-07-29 14:14:23 -0400

    Arreglo sensores

--- pruebaSensores/pruebaSensores.ino
+++ pruebaSensores/pruebaSensores.ino
@@ -10,13 +10,10 @@ BluetoothSerial SerialBT;
 
 
 QTRSensors qtr;
-QTRSensors qtrIzq;
-QTRSensors qtrDer;
 
 const uint8_t SensorCount = 6;
 uint16_t sensorValues[SensorCount];
-uint16_t sensorValuesIzq[1];
-uint16_t sensorValuesDer[1];
+
 
 #define LED 2
 
@@ -24,16 +21,8 @@ void setup() {
   // configure the sensors
   //siguelineas
   qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){ 25, 33, 32, 35, 34, 39 }, SensorCount);
-  qtr.setEmitterPin(27);
-  //sensor izquierdo
-  qtrIzq.setTypeAnalog();
-  qtrIzq.setSensorPins((const uint8_t[]){ 26 }, 1);
-  qtrIzq.setEmitterPin(27);
-  //sensor derecho
-  qtrDer.setTypeAnalog();
-  qtrDer.setSensorPins((const uint8_t[]){ 36 }, 1);
-  qtrDer.setEmitterPin(27);
+  qtr.setSensorPins((const uint8_t[]){ 26,25, 33, 32, 35, 34, 39, 36}, SensorCount);
+  qtr.setEmitterPin(27);  
   pinMode(LED,OUTPUT);
 
   for (int i = 0; i < 200; i++) {
@@ -51,23 +40,13 @@ void setup() {
 void loop() {
   // read raw sensor values
   qtr.read(sensorValues);
-  
-  int posicion = qtr.readLineBlack(sensorValues);
-  posicion = map(posicion,0,5000,-255,255);
-  Serial.print(posicion);
-  Serial.print('\t');
-  qtrIzq.read(sensorValuesIzq);
-  qtrDer.read(sensorValuesDer);
-  //int lecturaIzquierda = analogRead(26);
-  //int lecturaDerecha = analogRead(36);
-
-  // print the sensor values as numbers from 0 to 1023, where 0 means maximum
-  // reflectance and 1023 means minimum reflectance
-  Serial.print(sensorValuesIzq[0]);
-  Serial.print('\t');
   for (uint8_t i = 0; i < SensorCount; i++) {
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
-  Serial.println(sensorValuesDer[0]);  
+  int posicion = qtr.readLineBlack(sensorValues);
+  posicion = map(posicion,0,7000,-255,255);
+  Serial.print(posicion);
+  Serial.println('\t');
+  
 }

2beeb5080e5eb23e157ff758bf2cad8eda1ddb8d
Ben Espinoza <50683392+BenEspinoza10@users.noreply.github.com>
2026-04-02 18:23:21 -0300

    Unificacion motor

--- Codigos de prueba/pruebaMotores/pruebaMotores.ino
+++ Codigos de prueba/pruebaMotores/pruebaMotores.ino
@@ -11,19 +11,19 @@
 const int freq = 5000;
 const int resolution = 8;
 
-void Motoriz(int value) {
+void Motorde(int value) {
   if ( value >= 0 ) {
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, HIGH);
-  } else {
     digitalWrite(BIN1, HIGH);
     digitalWrite(BIN2, LOW);
+  } else {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
     value *= -1;
   }
   ledcWrite(0, value);
 }
 // Función accionamiento motor derecho
-void Motorde(int value) {
+void Motoriz(int value) {
   if ( value >= 0 ) {
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
@@ -45,12 +45,12 @@ void setup() {
   Serial.begin(115200);
   pinMode(BIN2  , OUTPUT);
   pinMode(BIN1  , OUTPUT);
-  ledcSetup(1, freq, resolution);
-  ledcAttachPin(PWMB, 1);
+  ledcSetup(0, freq, resolution);
+  ledcAttachPin(PWMB, 0);
   pinMode(AIN1  , OUTPUT);
   pinMode(AIN2  , OUTPUT);
-  ledcSetup(0, freq, resolution);
-  ledcAttachPin(PWMA, 0);
+  ledcSetup(1, freq, resolution);
+  ledcAttachPin(PWMA, 1);
   Serial.println("Presiona el boton para mover los motores");
   while (digitalRead(BOTON) == 0) {
   }
@@ -58,12 +58,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Motor(255,255);
+  Motor(150,150);
+  delay(1000);
   Serial.println("Moviendo ruedas");
-  /*Motor(-150,150);
+  Motor(0,0);
+  delay(200);
+  Motor(-150,150);
   delay(1000);
   Motor(0,0);
-  delay(1000);
+  delay(200);
   Motor(150,-150);
-  delay(1000);*/
+  delay(1000);
+  Motor(0,0);
+  delay(200);
+  Motor(-150,-150);
+  delay(1000);
+  Motor(0,0);
+  delay(200);
+  while (digitalRead(BOTON) == 0) {
+  }
 }

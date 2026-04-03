462c41b71d60a52143894fad8125a621b0f69bde
BenEspinoza10 <50683392+BenEspinoza10@users.noreply.github.com>
2026-04-03 14:39:29 -0300

    Update motores
    Ahora todo los motores funcionan bien

--- Codigos de clases/Clase 4/SigueLineasPosRelativa/Motores.ino
+++ Codigos de clases/Clase 4/SigueLineasPosRelativa/Motores.ino
@@ -3,27 +3,27 @@
 void inicializarMotores(){
   pinMode(BIN2  , OUTPUT);
   pinMode(BIN1  , OUTPUT);
-  ledcSetup(1, freq, resolution);
-  ledcAttachPin(PWMB, 1);
+  ledcSetup(0, freq, resolution);
+  ledcAttachPin(PWMB, 0);
   pinMode(AIN1  , OUTPUT);
   pinMode(AIN2  , OUTPUT);
-  ledcSetup(0, freq, resolution);
-  ledcAttachPin(PWMA, 0);
+  ledcSetup(1, freq, resolution);
+  ledcAttachPin(PWMA, 1);
 }
 
-void Motoriz(int value) {
+void Motorde(int value) {
   if ( value >= 0 ) {
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, HIGH);
-  } else {
     digitalWrite(BIN1, HIGH);
     digitalWrite(BIN2, LOW);
+  } else {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
     value *= -1;
   }
-  ledcWrite(1, value);
+  ledcWrite(0, value);
 }
 // Función accionamiento motor derecho
-void Motorde(int value) {
+void Motoriz(int value) {
   if ( value >= 0 ) {
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
@@ -32,7 +32,7 @@ void Motorde(int value) {
     digitalWrite(AIN2, HIGH);
     value *= -1;
   }
-  ledcWrite(0, value);
+  ledcWrite(1, value);
 }
 
 //Accionamiento de motores
